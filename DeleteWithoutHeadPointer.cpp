ec399dc7fda5213daece5ff09bd1a8c0c4a8cda8
Dishan Purkayastha <69187502+dishanp@users.noreply.github.com>
2021-05-24 10:50:35 +0530

    Create DeleteWithoutHeadPointer.cpp

---
+++ DeleteWithoutHeadPointer.cpp
@@ -0,0 +1,17 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode *p=node->next;
+        node->val=p->val;
+        node->next=p->next;
+        delete(p);
+    }
+};
