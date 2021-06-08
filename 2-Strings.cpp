173c6d74d3aaec8659b20fa83a792e9d91922fff
Dishan Purkayastha <69187502+dishanp@users.noreply.github.com>
2021-06-08 11:32:36 +0530

    Create 2-Strings.cpp

---
+++ 2-Strings.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'twoStrings' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. STRING s1
+ *  2. STRING s2
+ */
+
+string twoStrings(string s1, string s2) {
+    unordered_map<char,int>u;
+    for(unsigned int i=0;i<s1.size();i++){
+        u[s1[i]]++;
+    }
+    for(int i=0;i<s2.size();i++){
+        if(u[s2[i]]>0)
+        return "YES";
+    }
+    return "NO";   
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string q_temp;
+    getline(cin, q_temp);
+
+    int q = stoi(ltrim(rtrim(q_temp)));
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string s1;
+        getline(cin, s1);
+
+        string s2;
+        getline(cin, s2);
+
+        string result = twoStrings(s1, s2);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
