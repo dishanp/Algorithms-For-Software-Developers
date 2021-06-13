48ae803ddd7f3b14a3d03e46f2b5d25c97a1b724
Dishan Purkayastha <69187502+dishanp@users.noreply.github.com>
2021-06-13 11:30:26 +0530

    Create Reverse Queue.cpp

---
+++ Reverse Queue.cpp
@@ -0,0 +1,13 @@
+queue<int> rev(queue<int> q)
+{
+    stack<int>s;
+    while(!q.empty()){
+        s.push(q.front());
+        q.pop();
+    }
+    while(!s.empty()){
+        q.push(s.top());
+        s.pop();
+    }
+    return q;
+}
