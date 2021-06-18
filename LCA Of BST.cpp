2a0c67b94de7b1b7b108305e3d2c4d5f86c1a8ad
Dishan Purkayastha <69187502+dishanp@users.noreply.github.com>
2021-06-18 11:00:11 +0530

    Create LCA Of BST.cpp

---
+++ LCA Of BST.cpp
@@ -0,0 +1,12 @@
+Node* LCA(Node *root, int n1, int n2)
+{
+   if(!root)
+   return nullptr;
+   if(root->data==n1&&root->data==n2)
+   return root;
+   if(root->data>n1 && root->data>n2)
+   return LCA(root->left,n1,n2);
+   if(root->data<n1&&root->data<n2)
+   return LCA(root->right,n1,n2);
+   return root;
+}
