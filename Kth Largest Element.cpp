fc8239563916fae804390395cedb886d03dda52a
Dishan Purkayastha <69187502+dishanp@users.noreply.github.com>
2021-07-04 10:36:53 +0530

    Create Kth Largest Element.cpp

---
+++ Kth Largest Element.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        return nums[nums.size()-k];
+    }
+};  
+
+//OR
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+         priority_queue<int , vector<int> , greater<int>> minheap;
+        for(int i=0;i<nums.size();i++){
+            minheap.push(nums[i]);
+            if(minheap.size()>k)
+                minheap.pop();
+        }
+        return minheap.top();
+    }
+};
