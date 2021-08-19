b7b4622c2b03b3b948219fd644d82040d2319589
Dishan Purkayastha <69187502+dishanp@users.noreply.github.com>
2021-08-19 17:48:38 +0530

    Create Shortest Common Supersequence.cpp

---
+++ Shortest Common Supersequence.cpp
@@ -0,0 +1,29 @@
+    int util(string &S1, string& S2, int n, int m)
+    {
+        int res = 0;
+        
+        int dp[n + 1][m + 1];
+        
+        for(int i = 0; i <= n; i++)
+        {
+            for(int j = 0; j <= m; j++)
+            {
+                if(i == 0 || j == 0)
+                {
+                    dp[i][j] = 0;
+                }
+                else if(S1[i - 1] == S2[j - 1])
+                {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                }
+                else
+                    dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
+            }
+        }
+        
+        return dp[n][m];
+    }
+    int shortestCommonSupersequence(string X, string Y, int m, int n)
+    {
+       return m+n-util(X,Y,m,n);
+    }
