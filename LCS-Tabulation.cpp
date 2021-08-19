64bb284ba6b9ce11b480befcbd479f2ee96897dd
Dishan Purkayastha <69187502+dishanp@users.noreply.github.com>
2021-08-19 12:35:00 +0530

    Create LCS-Tabulation.cpp

---
+++ LCS-Tabulation.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int m=text1.size();
+        int n=text2.size();
+        int dp[m+1][n+1];
+        for(int i=0;i<m+1;i++){
+            for(int j=0;j<n+1;j++){
+                if(i==0||j==0)
+                    dp[i][j]=0;
+                else if(text1[i-1]==text2[j-1])
+                    dp[i][j]=1+dp[i-1][j-1];
+                else 
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[m][n];
+    }
+};
