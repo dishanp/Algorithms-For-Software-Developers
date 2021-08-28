8bdb72ac9bfaf16ac61b40fd17b1fef35f064413
Dishan Purkayastha <69187502+dishanp@users.noreply.github.com>
2021-08-28 17:16:06 +0530

    Create Detect cycle in an undirected graph.cpp

---
+++ Detect cycle in an undirected graph.cpp
@@ -0,0 +1,40 @@
+class Solution {
+  public:
+        bool checkForCycle(int s, int V, vector<int> adj[], vector<int>& visited)
+    {
+        vector<int> parent(V, -1);
+     
+        // Create a queue for BFS
+        queue<pair<int,int>> q;
+     
+        visited[s] = true;
+        q.push({s, -1});
+     
+        while (!q.empty()) {
+     
+            int node = q.front().first;
+            int par = q.front().second;
+            q.pop();
+     
+            for (auto it : adj[node]) {
+                if (!visited[it]) {
+                    visited[it] = true;
+                    q.push({it, node});
+                }
+                else if (par != it)
+                    return true;
+            }
+        }
+        return false;
+    }
+	bool isCycle(int V, vector<int>adj[]){
+	    vector<int> vis(V, 0); 
+	    for(int i = 0;i<V;i++) {
+	        if(!vis[i]) {
+	            if(checkForCycle(i, V, adj, vis)) return true; 
+	        }
+	    }
+	    
+	    return false; 
+	}
+};
