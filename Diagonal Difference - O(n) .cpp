6a9353a5943f08b39a68f810abbb3b96c9e9c7d6
Dishan Purkayastha <69187502+dishanp@users.noreply.github.com>
2021-09-02 12:22:26 +0530

    Create Diagonal Difference - O(n) .cpp

---
+++ Diagonal Difference - O(n) .cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define MAX 100
+using namespace std;
+
+int difference(int arr[][MAX], int n)
+{
+	// Initialize sums of diagonals
+	int d1 = 0, d2 = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		d1 += arr[i][i];
+		d2 += arr[i][n-i-1];
+	}
+
+	// Absolute difference of the sums
+	// across the diagonals
+	return abs(d1 - d2);
+}
+
+int main()
+{
+	int n = 3;
+
+	int arr[][MAX] =
+	{
+		{11, 2, 4},
+		{4 , 5, 6},
+		{10, 8, -12}
+	};
+
+	cout << difference(arr, n);
+	return 0;
+}
