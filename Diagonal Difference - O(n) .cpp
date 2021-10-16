b86de9f0687f0f30d01139d3e440fa01739d288f
SangeethaPrabhu2021 <86423721+SangeethaPrabhu2021@users.noreply.github.com>
2021-10-16 09:34:40 +0530

    Update Diagonal Difference - O(n) .cpp
    Removed Initialization and inputed the elements if the array from the user!

--- Diagonal Difference - O(n) .cpp
+++ Diagonal Difference - O(n) .cpp
@@ -2,7 +2,7 @@
 #define MAX 100
 using namespace std;
 
-int difference(int arr[][MAX], int n)
+int difference(int arr[MAX][MAX], int n)
 {
 	// Initialize sums of diagonals
 	int d1 = 0, d2 = 0;
@@ -13,6 +13,7 @@ int difference(int arr[][MAX], int n)
 		d2 += arr[i][n-i-1];
 	}
 
+
 	// Absolute difference of the sums
 	// across the diagonals
 	return abs(d1 - d2);
@@ -20,15 +21,16 @@ int difference(int arr[][MAX], int n)
 
 int main()
 {
-	int n = 3;
+	int n ;//COnsidering Diagonals only for Square Matrix
+	cout<<"Enter the no.of columns";
+	cin>>n;
 
-	int arr[][MAX] =
-	{
-		{11, 2, 4},
-		{4 , 5, 6},
-		{10, 8, -12}
-	};
+	int arr[MAX][MAX];
+	for(int i=0 ;i<n;i++)
+	for(int j=0;j<n;j++)
+	cin>>arr[i][j];
 
-	cout << difference(arr, n);
-	return 0;
+	cout <<"The Diffrence is"<<difference(arr, n);
+return 0;
+
 }
