ab137132931f70d8e29a72ba382a23f83f92fa39
Dishan Purkayastha <69187502+dishanp@users.noreply.github.com>
2021-10-24 10:15:43 +0530

    Create Prim's Minimum Spanning Tree.cpp

---
+++ Prim's Minimum Spanning Tree.cpp
@@ -0,0 +1,40 @@
+  int minSpan(int N,vector<vector<int>>adj[])
+  {
+  int parent[N+1]; 
+         
+       int key[N+1]; 
+         
+       bool mstSet[N+1]; 
+     
+       for (int i = 0; i <= N; i++) 
+           key[i] = INT_MAX, mstSet[i] = false; 
+       
+       priority_queue< pair<int,int>, vector <pair<int,int>> , greater<pair<int,int>> > pq;
+   
+       key[0] = 0; 
+       parent[0] = -1; 
+       pq.push({0, 0});
+       while(!pq.empty())
+       { 
+           int u = pq.top().second; 
+           pq.pop(); 
+           
+           mstSet[u] = true; 
+           
+           for (auto it : adj[u]) {
+               int v = it[0];
+               int weight = it[1];
+               if (mstSet[v] == false && weight < key[v]) {
+                   parent[v] = u;
+             key[v] = weight; 
+                   pq.push({key[v], v});    
+               }
+           }
+               
+       }
+       int sum = 0;
+       for(int i = 1;i<=N;i++) {
+           if(key[i] != INT_MAX) sum += key[i];
+       }
+       return sum;
+       }
