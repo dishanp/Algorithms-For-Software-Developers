33406b1f4af8c4397867480089d0023518fd0f97
Dishan Purkayastha <69187502+dishanp@users.noreply.github.com>
2021-11-12 11:46:58 +0530

    Create Union of two arrays.cpp

---
+++ Union of two arrays.cpp
@@ -0,0 +1,12 @@
+class Solution{
+    public:
+    //Function to return the count of number of elements in union of two arrays.
+    int doUnion(int a[], int n, int b[], int m)  {
+        unordered_set<int>s;
+        for(int i=0;i<n;i++)
+            s.insert(a[i]);
+        for(int j=0;j<m;j++)
+            s.insert(b[j]);
+        return s.size();
+    }
+};
