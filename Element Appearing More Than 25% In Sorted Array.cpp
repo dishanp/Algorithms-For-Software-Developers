124a53fe982f2631ad4eb56f08954779d63ba850
Dishan Purkayastha <69187502+dishanp@users.noreply.github.com>
2021-12-26 10:02:39 +0530

    Optimized Space Complexity to O(1)

--- Element Appearing More Than 25% In Sorted Array.cpp
+++ Element Appearing More Than 25% In Sorted Array.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
-    int findSpecialInteger(vector<int>& arr) {
-       unordered_map<int,int>u;
-        for(int i:arr)
-            u[i]++;
-        int f=arr.size()/4;
-        for(auto i:u)
-            if(i.second>f)
-                return i.first;
-        return -1;
-    }
+	int findSpecialInteger(vector<int>& arr) {
+		int c = 0;
+		for(int i = 1; i < arr.size(); i++){
+			if(arr[i] == arr[i - 1]){
+				c++;
+			}
+			else{
+				c = 1;
+			}
+			if(c > arr.size() / 4){
+				return arr[i];
+			}
+		}
+		return arr[0];
+	}
 };
