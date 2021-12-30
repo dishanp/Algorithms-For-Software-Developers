a2df62056c2635eb667374791d9a7e53fb68bd46
Dishan Purkayastha <69187502+dishanp@users.noreply.github.com>
2021-12-30 11:17:40 +0530

    Create Stack using 1 queue.cpp

---
+++ Stack using 1 queue.cpp
@@ -0,0 +1,35 @@
+class MyStack {
+public:
+    /** Initialize your data structure here. */
+    queue<int> q1;
+    MyStack() {
+        
+    }
+    
+    /** Push element x onto stack. */
+    void push(int x) {
+        int sz = q1.size();
+        q1.push(x);
+        while(sz--){
+            q1.push(q1.front());
+            q1.pop();
+        }
+    }
+    
+    /** Removes the element on top of the stack and returns that element. */
+    int pop() {
+        int result = top();
+        q1.pop();
+        return result;
+    }
+    
+    /** Get the top element. */
+    int top() {
+        return q1.front();
+    }
+    
+    /** Returns whether the stack is empty. */
+    bool empty() {
+        return q1.empty();
+    }
+};
