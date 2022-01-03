649a7965953950fea093aab097125a7d2c1dff5e
Dishan Purkayastha <69187502+dishanp@users.noreply.github.com>
2022-01-03 12:59:26 +0530

    optimal code format

--- Largest subarray with 0 sum.cpp
+++ Largest subarray with 0 sum.cpp
@@ -1,23 +1,24 @@
 class Solution{
     public:
-    int maxLen(vector<int>&a, int n)
+    int maxLen(vector<int>&arr, int n)
     {   
-        int ctr=0;
-        int sum=0;
-        unordered_map<int,int>u;
-        for(int i=0;i<n;i++){
-            sum+=a[i];
-            if(sum==0){
-                ctr=i+1;
-            }else{
-                if(u.find(sum)!=u.end()){
-                    ctr=max(ctr,i-u[sum]);
-                }
-                else{
-                    u[sum]=i;
-                }
-            }
-        }
-        return ctr;
+    int max_size = 0; //It will store the maxsize of subarray having sum=0
+    int prefix_sum = 0;
+    unordered_map<int, int> first_occ;
+    //This hashmap will contain the 'first occurnce' of the element.
+
+    first_occ[0] = -1;
+
+    for (int i = 0; i < n; i++)
+    {
+        prefix_sum += arr[i];
+        if (first_occ.find(prefix_sum) == first_occ.end()) //if prefix_sum, doesnot exist in hashmap
+            first_occ[prefix_sum] = i;                     // setting it's first occurance
+
+        int curr_size = i - first_occ[prefix_sum];
+        max_size = max(max_size, curr_size);
+    }
+
+    return max_size;
     }
 };
