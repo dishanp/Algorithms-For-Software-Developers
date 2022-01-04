7518a8fb89441b1b3f87cbc1c92e80fbee568635
Dishan Purkayastha <69187502+dishanp@users.noreply.github.com>
2022-01-04 12:48:12 +0530

    added tabular dp approach

--- Climb Stairs.cpp
+++ Climb Stairs.cpp
@@ -1,3 +1,5 @@
+// dp memoization - top down approach
+
 class Solution {
 public:
     int climbStairs(int n) {
@@ -12,5 +14,20 @@ public:
         if(n==2)
             return dp[n]=2;
         return dp[n]=util(n-1,dp)+util(n-2,dp);
+    }
+};
+
+// DP tabulation - bottom up approach
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int>dp(50,-1);
+        dp[0]=1;
+        dp[1]=2;
+        for(int i=2;i<n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n-1];
     }
 };
