29ebbe920835039f6a53c89406dd69423abadd8a
Dishan Purkayastha <69187502+dishanp@users.noreply.github.com>
2022-01-15 10:55:50 +0530

    added binary search soln.

--- PeakElement.cpp
+++ PeakElement.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
-    int findPeakElement(vector<int>& nums) {
-        int in=-1;
-        int n=nums.size();
-        if(n==1)
-            return 0;
-        if(nums[0]>nums[1])
-            return 0;
-        if(nums[n-1]>nums[n-2])
-            return n-1;
-        for(int i=1;i<n-1;i++){
-            if(nums[i]>nums[i+1]&&nums[i]>nums[i-1])
-            {
-                in=i;
-                break;
-            }
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int low = 0, high = arr.size()-1;
+        int mid;
+        while (low <= high) {
+            mid = low + (high - low) / 2;
+            if (arr[mid] < arr[mid+1])
+                low = mid+1;
+            else
+                high = mid-1;
         }
-        return in;
+        return low;
     }
 };
