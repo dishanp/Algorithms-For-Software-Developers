4d480dbde0dbf6dd66a3622acf69b49f1c8086d5
Dishan Purkayastha <69187502+dishanp@users.noreply.github.com>
2022-02-10 09:07:18 +0530

    Create Subarray With 0 Sum.cpp

---
+++ Subarray With 0 Sum.cpp
@@ -0,0 +1,50 @@
+// { Driver Code Starts
+// A C++ program to find if there is a zero sum
+// subarray
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    //Complete this function
+    //Function to check whether there is a subarray present with 0-sum or not.
+    bool subArrayExists(int arr[], int n)
+    {
+        int k=0;
+        map<int,int>m;
+        m[0]=1;
+        int ctr=0;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+            ctr+=m[sum-k];
+            m[sum]++;
+        }
+        return ctr>0;
+    }
+};
+
+// { Driver Code Starts.
+// Driver code
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int arr[n];
+	    for(int i=0;i<n;i++)
+	    cin>>arr[i];
+	    Solution obj;
+	    	if (obj.subArrayExists(arr, n))
+		cout << "Yes\n";
+	else
+		cout << "No\n";
+	}
+	return 0;
+}  // } Driver Code Ends
